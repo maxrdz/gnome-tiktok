c0e519c339583475e65dd21423d30aec0a38abca
maxrdz <me@maxrdz.com>
2024-01-30 15:01:17 -0700

    git: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gtktok
+
+A Linux mobile friendly app for using the Tiktok platform. Tested on the Phosh shell, a GNOME based mobile shell. Released under the Affero GNU General Public License version 3.
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+// Copyright (c) 2024, Gtktok Authors.
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License version 3.
+// You should have received a copy of this license along
+// with this source code in a file named "LICENSE."
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use glib::clone;
+// glib and other dependencies are re-exported by the gtk crate
+use gtk::glib;
+use gtk::prelude::*;
+
+// When the application is launched…
+fn on_activate(application: &gtk::Application) {
+    // … create a new window …
+    let window = gtk::ApplicationWindow::new(application);
+    // … with a button in it …
+    let button = gtk::Button::with_label("Hello World!");
+    // … which closes the window when clicked
+    button.connect_clicked(clone!(@weak window => move |_| window.close()));
+    window.set_child(Some(&button));
+    window.present();
+}
+
+fn main() {
+    // Create a new application with the builder pattern
+    let app = gtk::Application::builder()
+        .application_id("com.github.maxrdz.tiktok-linux-mobile")
+        .build();
+    app.connect_activate(on_activate);
+    // Run the application
+    app.run();
+}
+
