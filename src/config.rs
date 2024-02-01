6d3e0a6030cf68f94f61bdd8b4470f8ce264d60d
maxrdz <me@maxrdz.com>
2024-02-01 00:47:17 -0700

    meson: Fixed release and dev builds & installation

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
 pub static VERSION: &str = "0.1.0";
 pub static GETTEXT_PACKAGE: &str = "gnome-tiktok";
-pub static LOCALEDIR: &str = "/app/share/locale";
-pub static PKGDATADIR: &str = "/app/share/gnome-tiktok";
+pub static LOCALEDIR: &str = "/usr/local/share/locale";
+pub static PKGDATADIR: &str = "/usr/local/share/gnome-tiktok";
